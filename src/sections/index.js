23bd327f201f838cceaa3f37859bf2c231c8ec4c
MrUnAnonymous <saurabhsapkal16@gmail.com>
2023-10-31 15:15:19 +0530

    added navbar and hero section

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import Footer from './Footer';
+import CustomerReview from './CustomerReview';
+import Services from './Services';
+import SuperQuality from './SuperQuality';
+import SpecialOffer from './SpecialOffer';
+import PopularProducts from './PopularProducts';
+import Subscribe from './Subscribe';
+
+export {
+    Hero,
+    Footer,
+    CustomerReview,
+    Services,
+    SpecialOffer,
+    SuperQuality,
+    PopularProducts,
+    Subscribe
+}
