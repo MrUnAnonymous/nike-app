e84a0d9047ec3dd7880577d74c64762aefd052f3
MrUnAnonymous <saurabhsapkal16@gmail.com>
2023-10-31 23:49:20 +0530

    resolved bug

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,4 @@
-const Buttons = ({ label, iconURL }) => {
+const Button = ({ label, iconURL }) => {
   return (
     <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
        {label}
@@ -7,4 +7,4 @@ const Buttons = ({ label, iconURL }) => {
   )
 }
 
-export default Buttons
+export default Button;
